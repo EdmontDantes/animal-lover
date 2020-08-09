08ccf26e974a8a3d45db7397916c8efffa6453e8
EdmontDantes <bogdan.kowaltchook@gmail.com>
2020-08-09 19:46:36 -0400

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class App extends Component {
+  state = {
+
+  }
+
+  render() {
+    return(
+      <div>
+      <h1>Hello Begining of Animal Lover Assignment Homework</h1>
+      </div>
+    )
+  }
+}
+
+export default App
